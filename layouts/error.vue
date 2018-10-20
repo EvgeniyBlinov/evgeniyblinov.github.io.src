bf2d487c3e1c45688b5ae695bfe224ffa1c60b08
Blinov Evgeniy <evgeniy_blinov@mail.ru>
2018-10-20 17:48:28 +0300

    master
    Work stage 2018-10-20 17:48:28
    A  layouts/error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <div v-if="error.statusCode === 404">
+        <h1 class="center">Page not found</h1>
+        <h2 class="center">404</h2>
+    </div>
+    <h1 v-else>An error occurred</h1>
+    <nuxt-link to="/">Home page</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error'],
+  layout: 'default'
+}
+</script>
